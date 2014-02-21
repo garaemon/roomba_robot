e7623609ee47e769a5be47187c3dc62bf5eaea1a
rosuser <rosuser@rosueda.csg.ci.i.u-tokyo.ac.jp>
2014-02-21 14:26:03 +0900

    support led and digits

--- roomba_sample/scripts/joy.py
+++ roomba_sample/scripts/joy.py
@@ -3,8 +3,10 @@
 import rospy
 
 from geometry_msgs.msg import Twist
-from sensor_msgs.msg import Joy
-
+from sensor_msgs.msg import Joy 
+from nav_msgs.msg import Odometry
+from roomba_500_series.msg import DigitLeds
+import math
 twist_pub = None
 
 def joyCB(msg):
@@ -22,11 +24,32 @@ def joyCB(msg):
     twist.linear.x = -0.1
   twist_pub.publish(twist)
 
+def odomCB(msg):
+  global digits_pub
+  distance = math.sqrt(msg.pose.pose.position.x * msg.pose.pose.position.x + msg.pose.pose.position.y * msg.pose.pose.position.y)
+  # mm
+  print distance
+  distance_mm = distance
+  digits_nums = [int(math.floor(distance_mm / 1000.0)) % 10,
+                 int(math.floor(distance_mm / 100.0))  % 10,
+                 int(math.floor(distance_mm / 10.0))   % 10,
+                 int(math.floor(distance_mm / 1.0))      % 10]
+  print digits_nums
+  digits_codes = [ord('0') + v for v in reversed(digits_nums)]   #digits[0] => 1 order
+  digits = DigitLeds()
+  digits.header.stamp = rospy.Time.now()
+  digits.digits = digits_codes
+  digits_pub.publish(digits)
+  
+  
 def main():
-  global twist_pub
+  global twist_pub, digits_pub
   rospy.init_node("roomba_joy_sample") # declare my name
   twist_pub = rospy.Publisher("/cmd_vel", Twist)
+  digits_pub = rospy.Publisher("/digit_leds", DigitLeds)
   joy_sub = rospy.Subscriber("/joy", Joy, joyCB)
+  
+  odom_sub = rospy.Subscriber("/odom", Odometry, odomCB)
   rospy.spin()                        #
 
 if __name__ == "__main__":
